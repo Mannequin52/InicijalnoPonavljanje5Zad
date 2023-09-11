e52df58a450ca327c3865f7509d0b7db059d4d27
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-09-11 11:13:12 +0200

    Add files via upload

---
+++ Inicijalni5_4D/Inicijalni5_4D/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni5_4D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*Pojednostavljena provjera ispravnosti URL adrese registrirane u 
+          * hrvatskom internetskom prostoru bila bi:
+        • mora počinjati s www
+        • mora završavati s hr
+        • mora sadržavati točku nakon www i prije hr
+        Napišite program koji će zatražiti od korisnika da unese URL adresu, 
+            te će provjeriti je li adresa napisana u ispravnom formatu.
+        */
+
+            Console.WriteLine("Unesi URL adresu:");
+            string a = Console.ReadLine();
+            int duljina = a.Length;
+            int b = duljina - 3;
+            
+            string pocetak = a.Substring(0, 4);
+            string kraj = a.Substring(3, b);
+
+            if (pocetak == "www." && kraj == ".hr")
+            {
+                Console.WriteLine("URL je ispravan!");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
